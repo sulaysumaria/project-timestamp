2e34bd4e0488f718cd7fc7ba95950dab430f9901
Sulay Sumaria <sulaysumaria@gmail.com>
2019-09-07 06:00:02 +0530

    Updated deps.

--- server.js
+++ server.js
@@ -2,61 +2,64 @@
 // where your node app starts
 
 // init project
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-const cors = require('cors')
-app.use(cors()) // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors()); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-})
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-// your first API endpoint...
-app.get('/api/hello', (req, res) => {
-  res.json({ greeting: 'hello API' })
-})
+app.get('/api/timestamp/:dateString?', (req, res) => {
+  let {dateString = ''} = req.params;
 
-app.get('/api/timestamp/:date_string', (req, res) => {
-  let { date_string = '' } = req.params
+  let status = 500;
+  let response = {message: 'Internal Server Error'};
 
-  let status = 500
-  let response = { message: 'Internal Server Error' }
-
-  date_string = date_string.split('-')
-
-  console.log({ date_string })
-  if (!date_string.length) {
-    date_string = new Date()
-    status = 200
-    response = { unix: date_string.getTime(), utc: date_string.toUTCString() }
-  } else if (date_string.length === 1) {
-    date_string = new Date(parseInt(date_string[0], 10))
-    status = 200
-    response = { unix: date_string.getTime(), utc: date_string.toUTCString() }
-  } else if (date_string.length === 3) {
-    date_string = new Date(date_string.join('-'))
-    status = 200
-    response = { unix: date_string.getTime(), utc: date_string.toUTCString() }
+  if (!dateString) {
+    dateString = new Date();
+    status = 200;
+    response = {unix: dateString.getTime(), utc: dateString.toUTCString()};
   } else {
-    status = 400
-    response.message = 'Date might be in invalid format.'
+    dateString = dateString.split('-');
+
+    if (!dateString.length) {
+      dateString = new Date();
+      status = 200;
+      response = {unix: dateString.getTime(), utc: dateString.toUTCString()};
+    } else if (dateString.length === 1) {
+      dateString = new Date(parseInt(dateString[0], 10));
+      status = 200;
+      response = {unix: dateString.getTime(), utc: dateString.toUTCString()};
+    } else if (dateString.length === 3) {
+      dateString = new Date(dateString.join('-'));
+      status = 200;
+      response = {unix: dateString.getTime(), utc: dateString.toUTCString()};
+    } else {
+      status = 400;
+      response.message = 'Date might be in invalid format.';
+    }
   }
-  return res.status(status).send(response)
-})
+
+  return res.status(status).send(response);
+});
 
 // listen for requests :)
 app.listen(PORT, () => {
-  console.log('Your app is listening on port ' + PORT)
-})
+  console.log('Your app is listening on port ' + PORT);
+});
 
 // export app :)
-module.exports = { app }
+module.exports = {
+  app,
+};
